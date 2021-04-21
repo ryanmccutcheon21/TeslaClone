af2b61d8b22b8c227afc144e868c90c95127937d
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-21 11:46:41 -0500

    Added background image

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground
+          source={require('./assets/images/ModelX.jpeg')}
+          style={styles.image} />
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
@@ -43,5 +46,12 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 16,
     color: '#5c5e62',
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
   }
 });
